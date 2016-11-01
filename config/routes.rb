ab2cd6bd5eaafc55f3ef3672829f60dd0aa83fd9
Peter <peter@fullencpa.com>
2016-11-01 16:09:04 +0000

    Finnish user sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  root 'static_pages#home'  
-  get 'help'  => 'static_pages#help'
-  get 'about' => 'static_pages#about'
-  get 'signup'=> 'users#new'
+    
+    root 'static_pages#home'
+    get 'help'  => 'static_pages#home'
+    get 'about' => 'static_pages#help'
+    get 'contact' => 'static_pages#help'
+    get 'signup' => 'users#new'
+    resources :users
 end
