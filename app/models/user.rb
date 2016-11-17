1aa0c4ba47658b4bd6194c1422f19f78c8150fc9
Peter <peter@fullencpa.com>
2016-11-17 23:17:57 +0000

    feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -91,10 +91,10 @@ class User < ActiveRecord::Base
       #              OR user_id = :user_id", user_id: id)
   #end
   Def feed
-    following_ids = "SELECT followed_id FROM relationships
-                     WHERE  follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids})
-                    OR user_id = :user_id", user_id: id)
+  #  following_ids = "SELECT followed_id FROM relationships
+   #                  WHERE  follower_id = :user_id"
+    #Micropost.where("user_id IN (#{following_ids})
+     #               OR user_id = :user_id", user_id: id)
   end
   # Follows a user.
   #def follow(other_user)
