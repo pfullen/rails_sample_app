cde04dc556ce1dd849d6090e62c92c5eb24a819f
Peter <peter@fullencpa.com>
2016-11-17 15:43:49 +0000

    corrected email activation problems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
     attr_accessor :remember_token, :activation_token, :reset_token
-    before_save { self.email = email.downcase }
+    before_save :downcase_email
     before_create :create_activation_digest
     validates :name, presence: true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -39,7 +39,7 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, nil)
     end
     
-    def active
+    def activate
         update_attribute(:activated, true)
         update_attribute(:activated_at, Time.zone.now)
     end
